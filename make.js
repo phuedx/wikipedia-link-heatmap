8064b8cc852518ad445d03dab5d6d846b69c6dd8
Sam Smith <me@samsmith.io>
2018-03-28 14:12:48 +0100

    chore: Remove intermediate "client" files
    There's no need to render the client.js file and writing it to a file
    only then to copy that file to the clipboard.
    
    Rendering the client.js file directly to the clipboard makes the
    Popups_page_link_interactions.json and client.js intermediate files
    redundant.

--- make.js
+++ make.js
@@ -2,9 +2,61 @@
 //                 pasted into your browser's console
 
 const resolve = require('path').resolve
-const readFileSync = require('fs').readFileSync
 const clipboardy = require('clipboardy')
 
-const CLIENT_SRC = readFileSync(resolve('./client.js')).toString()
+const fs = require('fs')
+const parse = require('csv-parse')
 
-clipboardy.writeSync(CLIENT_SRC)
+const IN = resolve('./resources/Barack_Obama_link_interactions.tsv')
+const CLIENT_SRC_TPL = fs.readFileSync(resolve('./resources/client.js.tpl')).toString()
+
+const parser = parse({
+  delimiter: '\t'
+})
+
+const OPACITY_MAX = 0.75
+
+let result = {}
+let max = 0
+
+fs.createReadStream(IN)
+  .pipe(parser)
+  .on('data', (row) => {
+    row.shift() // Drop the wiki field.
+
+    const pageKey = `${row[0]}:${row[1]}`
+    let page = result[pageKey] || {
+      length: 0,
+      links: {}
+    }
+
+    const n = parseInt(row[4])
+
+    max = Math.max(max, n)
+
+    const linkKey = `${row[2]}:${row[3]}`
+    page.links[linkKey] = n
+
+    ++page.length
+
+    result[pageKey] = page
+  })
+  .on('finish', () => {
+    let page
+    let opacity
+
+    for (let pageKey in result) {
+      page = result[pageKey]
+
+      for (let linkKey in page.links) {
+        opacity = parseInt(page.links[linkKey]) / max
+        opacity *= OPACITY_MAX
+
+        page.links[linkKey] = opacity
+      }
+    }
+
+    clientSrc = CLIENT_SRC_TPL.replace('<%= DATA %>', JSON.stringify(result))
+
+    clipboardy.writeSync(clientSrc)
+  })
